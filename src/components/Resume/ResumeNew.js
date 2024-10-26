caac3f9097c9df8cee49b2aeb8f0f65fc0b06cd3
Hamada Taha <96458235+Hamadataha90@users.noreply.github.com>
2024-10-26 15:53:50 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,32 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Hamada_Elsayed.pdf";
+import pdf from "../../Assets/../Assets/Hamada_Elsayed.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-  const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
-
-  const handleNextPage = () => {
-    if (pageNumber < 2) {
-      setPageNumber(pageNumber + 1);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (pageNumber > 1) {
-      setPageNumber(pageNumber - 1);
-    }
-  };
 
   return (
     <div>
@@ -47,17 +33,8 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button onClick={handlePrevPage} disabled={pageNumber === 1}>
-            &lt; Previous
-          </Button>
-          <Button onClick={handleNextPage} disabled={pageNumber === 2}>
-            Next &gt;
-          </Button>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
