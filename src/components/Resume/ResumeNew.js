d919f342e0a9b1f71f96c10c9258095862237af0
Hamada Taha <96458235+Hamadataha90@users.noreply.github.com>
2024-10-26 15:56:39 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,18 +2,37 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Hamada_Elsayed.pdf"
+import pdf from "../../Assets/../Assets/Hamada_Elsayed.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
+  const handleNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
 
   return (
     <div>
@@ -32,9 +51,18 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess} className="d-flex justify-content-center">
+            <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button onClick={handlePrevPage} disabled={pageNumber === 1}>
+            &lt; Previous
+          </Button>
+          <Button onClick={handleNextPage} disabled={pageNumber === numPages}>
+            Next &gt;
+          </Button>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
